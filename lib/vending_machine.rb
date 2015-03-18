25bc2cb4ca82dfb189f5cde83baf1896b995c7c9
Gabriel Kasurinen <gabriel.kasurinen@elev.it-gymnasiet.se>
2015-03-18 11:14:54 +0100

    Made code

---
+++ lib/vending_machine.rb
@@ -0,0 +1,27 @@
+def handla(vara, kronor)
+  kronor = kronor.to_i
+  choklad = choklad
+  festis = festis
+  if vara == choklad
+
+    if kronor >= 10
+      elsif kronor > 10
+        return "tack tack"
+      end
+      puts "choklad"
+    end
+
+
+  if vara == festis
+  if kronor >= 8
+    elsif kronor > 8
+      return "tack tack"
+    end
+    return "festis"
+end
+end
+
+v = choklad
+k = 22
+
+handla(vara: v, kronor: k)
